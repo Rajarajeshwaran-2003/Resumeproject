09b9f20ed4127dc498525e8b48a1dab42691265c
Rajarajeshwaran-2003 <kannarajesh057@gmail.com>
2025-04-20 11:02:52 +0530

    Create static directory for collecting static files

--- ats_resume_generator/settings.py
+++ ats_resume_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,6 +100,12 @@ STATIC_URL = '/static/'  # This is good for serving static files
 STATICFILES_DIRS = [
     BASE_DIR / 'resumeapp' / 'static',  # Correct path to your static folder
 ]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where static files will be collected
 
